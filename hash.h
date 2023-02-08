4ad50d2d10e49d62c44c63d868459f4e3424a0c8
Los-merengue <michaels4uandme@gmail.com>
2023-02-08 15:13:04 +0300

    Added the possible vulnerabilities that would exist in the header file

--- hash.h
+++ hash.h
@@ -1,26 +1,27 @@
 /**
 *
 * @Name : hash.h
-*
+* This is a header file for the hash.c file, it contains some macro
+* definition of the functions in the c program.
 **/
 #ifndef __HASH__
 #define __HASH__
 
     typedef struct {
         #define KEY_STRING_MAX 255
-		char KeyName[KEY_STRING_MAX];
-		int  ValueCount;
-        struct PairValue* Next;
+		char KeyName[KEY_STRING_MAX];  //unsanitize KeyName data can lead to injections, and the
+		int  ValueCount;               //possibility that it could lead to buffer overflow, causing
+        struct PairValue* Next;        //unavalability of data.
 	} PairValue;
 
 	typedef struct {
         #define MAP_MAX 128
-		PairValue* data[MAP_MAX];
-	} HashMap;
+		PairValue* data[MAP_MAX]; //unsanitize data type can lead to injection,
+	} HashMap;                    //affecting confidentaility, integrity and availability
 
     HashMap* HashInit();
-    void HashAdd(HashMap *map, PairValue *value);
-    void HashDelete(HashMap *map, const char* key);
-    PairValue* HashFind(HashMap *map, const char* key);
+    void HashAdd(HashMap *map, PairValue *value); // lack of Error handling, affecting confidentiality
+    void HashDelete(HashMap *map, const char* key); // Inappropriate Data handling, leading to memory leaks and affecting integrity
+    PairValue* HashFind(HashMap *map, const char* key); //Potential buffer overflow
     void HashDump(HashMap *map);
 #endif
